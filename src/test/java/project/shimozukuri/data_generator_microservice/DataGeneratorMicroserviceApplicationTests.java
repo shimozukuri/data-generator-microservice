8d040db8f6632c2b7eabd87cf4c31ae36b86b37f
shimozukuri <whitelord228@yandex.ru>
2024-09-25 20:11:14 +0300

    first commit

---
+++ src/test/java/project/shimozukuri/data_generator_microservice/DataGeneratorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.shimozukuri.data_generator_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
