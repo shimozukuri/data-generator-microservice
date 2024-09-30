b7c9cd0ca7662cc5346afa50e71a8d8b79ccfcbe
shimozukuri <whitelord228@yandex.ru>
2024-09-30 19:33:51 +0300

    create DataTestOptions create DataTestOptionsDto

--- src/main/java/project/shimozukuri/data_generator_microservice/web/controller/DataController.java
+++ src/main/java/project/shimozukuri/data_generator_microservice/web/controller/DataController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.shimozukuri.data_generator_microservice.model.Data;
+import project.shimozukuri.data_generator_microservice.service.KafkaDataService;
 import project.shimozukuri.data_generator_microservice.web.dto.DataDto;
+import project.shimozukuri.data_generator_microservice.web.dto.DataTestOptionsDto;
 import project.shimozukuri.data_generator_microservice.web.mapper.DataMapper;
 
 @RestController
@@ -14,13 +16,18 @@ import project.shimozukuri.data_generator_microservice.web.mapper.DataMapper;
 @RequiredArgsConstructor
 public class DataController {
 
-//    private final KafkaDataService kafkaDataService;
-
+    private final KafkaDataService kafkaDataService;
     private final DataMapper dataMapper;
+    private final DataTestOptionsMapper dataTestOptionsMapper;
 
     @PostMapping("/send")
     public void send(@RequestBody DataDto dto) {
         Data data = dataMapper.toEntity(dto);
-//        kafkaDataService.send(data);
+        kafkaDataService.send(data);
+    }
+
+    @PostMapping("/test/send")
+    public void testSend(@RequestBody DataTestOptionsDto testOptionsDto) {
+
     }
 }
---
+++ src/main/java/project/shimozukuri/data_generator_microservice/web/dto/DataTestOptionsDto.java
@@ -0,0 +1,16 @@
+
+package project.shimozukuri.data_generator_microservice.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import project.shimozukuri.data_generator_microservice.model.Data;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class DataTestOptionsDto {
+
+    private int delayInSeconds;
+    private Data.MeasurementType[] measurementTypes;
+}
